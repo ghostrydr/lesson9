8bbfd578dd2ea3b1e789bce2b67a46bbac3ba13a
Russell Wilson <ghostrydr@users.noreply.github.com>
2025-05-27 16:51:35 +0000

    created app interface with package

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
+import { multiply, add, subtract, divide } from "@ghostrydr/lesson8/src/appOperations"
 
 function App() {
+
+  function get(id) {
+    return document.getElementById(id).value
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <input id="num1" />
+        <input id="num2" />
+        <button onClick={() => alert(multiply(get('num1'), get('num2')))}>Multiply</button>
+        <button onClick={() => alert(divide(get('num1'), get('num2')))}>Divide</button>
+        <button onClick={() => alert(add(get('num1'), get('num2')))}>Add</button>
+        <button onClick={() => alert(subtract(get('num1'), get('num2')))}>Subtract</button>
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
